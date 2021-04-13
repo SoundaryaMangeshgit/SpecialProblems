8c6043ce0114896e87294e30c97f4fda258ef0f1
SoundaryaMangeshgit <69762300+SoundaryaMangeshgit@users.noreply.github.com>
2021-04-13 17:35:08 -0500

    Add files via upload

---
+++ Twitter Visualization/server/app-server.js
@@ -0,0 +1,76 @@
+//Require Dependencies
+var express = require('express');
+
+var Twitter = require('./Twitter');
+var sentiment = require('./NodeSentiment.js');
+var twitterStream = Twitter;
+
+//Create Express Server Instance
+var app = express();
+var port = process.env.PORT || 4000;
+
+//Routes and Serve Static Files
+app.use(express.static(__dirname + './../app/public'));
+
+
+
+/*
+Create Server and Socket.io Instance
+==================*/
+var server = app.listen(port);
+console.log('Server on port: %s', port);
+
+var io = require('socket.io').listen(server);
+var connections = [];
+
+//Create socket.io Connection with Client
+//All Socket Listeners Here
+io.sockets.on('connection', function(socket) {
+
+  connections.push(socket);
+  console.log('%s Connected. %s sockets connected', socket.id, connections.length);
+  
+  var prevSearch = false;
+  var twitterStream;
+
+  socket.on('search', function(payload) {
+    console.log('Keyword: %s', payload.keyword);
+
+    if(prevSearch) {
+      twitterStream.stop();
+      console.log(prevSearch);
+      console.log('stop stream');
+
+    } else {
+      prevSearch = true;
+    }
+
+    twitterStream = Twitter.stream('statuses/filter', {language: 'en', track: payload.keyword});
+
+
+    var lastTimestamp = Date.now(),
+        speedLimiter = 800; //800ms
+
+    //Turn on Twitter Stream
+    twitterStream.on('tweet', function(tweet) {
+
+      if(tweet.timestamp_ms - lastTimestamp > speedLimiter) {
+
+        lastTimestamp = Date.now();
+
+        // Send Tweet Object to Client
+        socket.emit('sendTweet', {tweet: sentiment.getSentiment(tweet, socket)});
+      }
+    });
+
+    socket.once('disconnect', function() {
+      connections.splice(connections.indexOf(socket), 1);
+      socket.disconnect();
+      twitterStream.stop();
+      console.log('Socket disconnected: %s sockets remaining', connections.length);
+    });
+  });
+
+}); //END io.sockets.on
+
+
